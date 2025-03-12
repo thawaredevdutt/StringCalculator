db26b55ff4b5fc50f9ae9d5b472931d9387fc0d6
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 20:02:06 +0530

    add scenerio for numbers string

--- lib/spec/calculator_spec.rb
+++ lib/spec/calculator_spec.rb
@@ -2,8 +2,14 @@ require_relative '../calculator'
 
 RSpec.describe Calculator do
   describe '.add' do
-    it 'returns 0 for an empty string' do
-      expect(Calculator.add("")).to eq(0)
+    context 'with valid input' do
+      it 'returns 0 for an empty string' do
+        expect(Calculator.add("")).to eq(0)
+      end
+
+      it 'returns the number itself for a single number' do
+        expect(Calculator.add("1")).to eq(1)
+      end
     end
   end
 end
