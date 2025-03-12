46f088faca9d175ca5019a49ecaf07b4743a5b4c
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 19:56:43 +0530

    add scenerio for empty string

--- lib/spec/calculator_spec.rb
+++ lib/spec/calculator_spec.rb
@@ -1,5 +1,9 @@
 require_relative '../calculator'
 
 RSpec.describe Calculator do
-
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(Calculator.add("")).to eq(0)
+    end
+  end
 end
