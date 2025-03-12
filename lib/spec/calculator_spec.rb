cd47a897c5b3b87b355545cf20e0c5760bfc7ab7
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 20:04:32 +0530

    test scenerio with multi number string

--- lib/spec/calculator_spec.rb
+++ lib/spec/calculator_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Calculator do
       it 'returns the number itself for a single number' do
         expect(Calculator.add("1")).to eq(1)
       end
+
+      it 'returns the sum of numbers for comma-separated numbers' do
+        expect(Calculator.add("1,5")).to eq(6)
+      end
     end
   end
 end
