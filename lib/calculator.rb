038b5abfab73ba1fa5476741b33dd6d352a009ae
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 20:14:01 +0530

    cover scenerio for negative number validation

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,11 +2,22 @@
 
 class Calculator
   def self.add(numbers)
-    return 0 if numbers.empty?+    @string_number = numbers
-    numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
+    evaluate_string(numbers)
   end
 
   private
+
+  def self.evaluate_string(string_number)
+    return 0 if string_number.empty?
+
+    @string_number = string_number
+
+    numbers = @string_number.split(/#{delimiter}|\n/).map(&:to_i)
+    negatives = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+    numbers.sum
+  end
 
   def self.delimiter
     str = ','
