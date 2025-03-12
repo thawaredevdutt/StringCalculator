68ca35c5b55842bdff8a6f0ad0009cceaaf0e84e
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 20:19:42 +0530

    refactore code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
 class Calculator
-  def self.add(numbers)
-    evaluate_string(numbers)
-  end
+  class << self
+    def add(numbers)
+      return 0 if numbers.empty?
 
-  private
-
-  def self.evaluate_string(string_number)
-    return 0 if string_number.empty?
-
-    @string_number = string_number
-    validate_numbers
-  end
-
-  def self.validate_numbers
-    return 'Invalid Input' unless valid_input?
-
-    numbers = @string_number.split(/#{delimiter}|\n/).map(&:to_i)
-    negatives = numbers.select(&:negative?)
-    raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
-
-    numbers.sum
-  end
-
-  def self.valid_input?
-    numbers = @string_number.split(delimiter)
-    numbers.select { |num| num.eql?("\n") }.empty?
-  end
-
-  def self.delimiter
-    str = ','
-    if @string_number.start_with?('//')
-      str, _numbers = @string_number.split("\n", 2)
-      str = str[2..]
+      evaluate_string(numbers)
     end
-    str    numbers.split(/,|\n/).map(&:to_i).sum
+
+    private
+
+    def evaluate_string(string_number)
+      @string_number = string_number
+      return 'Invalid Input' unless valid_input?
+
+      validate_numbers
+    end
+
+    def validate_numbers
+      numbers = @string_number.split(/#{delimiter}|\n/).map(&:to_i)
+      negatives = numbers.select(&:negative?)
+      raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+      numbers.sum
+    end
+
+    def valid_input?
+      numbers = @string_number.split(delimiter)
+      numbers.select { |num| num.eql?("\n") }.empty?
+    end
+
+    def delimiter
+      str = ','
+      if @string_number.start_with?('//')
+        str, _numbers = @string_number.split("\n", 2)
+        str = str[2..]
+      end
+      str
+    end
   end
 end
