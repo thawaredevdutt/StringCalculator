1e2e3782985b0939240ef995e5c94b0ffa176ba2
thawaredevdutt <thawaredevdutt07@gmail.com>
2025-03-12 20:08:33 +0530

    add scenerio for custom delimiters

--- lib/spec/calculator_spec.rb
+++ lib/spec/calculator_spec.rb
@@ -18,6 +18,10 @@ RSpec.describe Calculator do
       it 'handles new lines between numbers' do
         expect(Calculator.add("1\n2,3")).to eq(6)
       end
+
+      it 'supports custom delimiters' do
+        expect(Calculator.add("//;\n1;2")).to eq(3)
+      end
     end
   end
 end
